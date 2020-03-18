b5e15279cd14fe0cdd159b767a665edf77e1645a
sacts <cheemm9@mac1xa3.ca>
2020-03-18 21:45:24 +0000

    Submitting Project 02

---
+++ Project02/resume1.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".ibtehaaj0").click(function(){
+        $("#haha").toggle("slow");
+      });
+      $(".ibtehaaj1").click(function(){
+        $("#haha1").toggle("slow");
+      });
+      $(".ibtehaaj2").click(function(){
+        $("#haha2").toggle("slow");
+      }); 
+      $(".supper0").click(function(){
+        $(".fazool").toggle("slow");
+      });
+      $(".supper1").click(function(){
+        $(".fazool1").toggle("slow");
+      });    
+ });
